7ed457978cacb928c39e89bec39218da3b2db3e2
Veda Sai Priya <2100031238@kluniversity.in>
2023-09-10 11:14:42 +0530

    added update,delete in CRUD operations

--- myproject/app1/urls.py
+++ myproject/app1/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import home, login,insert_emp,register,view_all
+from .views import register,login,home,insert_emp,view_all,update_emp,delete_emp
 
 urlpatterns = [
     path('', register),
     path('login/', login),
     path('home/', home),
     path('insert/',insert_emp),
-    path('viewall/',view_all)
+    path('viewall/',view_all,name="show-emp"),
+    path('update/<int:pk>',update_emp),
+    path('delete/<int:pk>',delete_emp),
+
 ]
--- myproject/app1/views.py
+++ myproject/app1/views.py
@@ -30,16 +30,45 @@ def home(request):
 
 
 def insert_emp(request):
-    eid = request.POST.get('eid')
-    ename = request.POST.get('ename')
-    email = request.POST.get('email')
-    gender = request.POST.get('gender')
-    desg = request.POST.get('desg')
-    emp = Employee.objects.create(empid=eid, name=ename, gender=gender, email=email, designation=desg)
-    emp.save()
-    return redirect('/home/')
+
+    if request.method == 'POST':
+        eid = request.POST.get('eid')
+        ename = request.POST.get('ename')
+        email = request.POST.get('email')
+        gender = request.POST.get('gender')
+        desg = request.POST.get('desg')
+        emp = Employee.objects.create(empid=eid, name=ename, gender=gender, email=email, designation=desg)
+        emp.save()
+        return redirect('/viewall/')
+
+    return render(request, 'insert.html')
 
 
 def view_all(request):
     emp = Employee.objects.all()
     return render(request, 'display.html', {'employees': emp})
+
+
+def update_emp(request,pk):
+    emp = Employee.objects.get(id=pk)
+
+    if request.method == 'POST':
+        emp.empid = request.POST.get('eid')
+        emp.name = request.POST.get('ename')
+        emp.email = request.POST.get('email')
+        emp.gender = request.POST.get('gender')
+        emp.designation = request.POST.get('desg')
+        emp.save()
+        return redirect('/viewall/')
+
+    return render(request, 'update.html',{'employee': emp})
+
+
+def delete_emp(request,pk):
+    emp = Employee.objects.get(id=pk)
+
+    if request.method == 'POST':
+        emp.delete()
+        return redirect('/viewall/')
+
+    return render(request, 'delete.html',{'employee': emp})
